6bbfaa5a7312cdf09c38e1a8f8170a69c7843f73
TEV <leeloun@naver.com>
2024-11-05 11:14:57 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.title("chat bot - test")
 st.text("국영수 중 한 과목을 입력해주세요 ")
 
 
-input = st.text_input("메시지를 입력하세요:")
+input = st.text_input("과목을 입력하세요:")
 if st.button("챗봇에게 보내기"):
     # 간단한 응답을 제공하는 챗봇 기능 예제
     if input:
@@ -19,4 +19,4 @@ if st.button("챗봇에게 보내기"):
         
         st.write("영어 번역: ", response)
     else:
-        st.write("메시지를 입력하세요")
+        st.write("국어, 영어 수학 중 한 과목을 입력해주세요")
